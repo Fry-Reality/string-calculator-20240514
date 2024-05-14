6d31e4c952c90b49e04f324ec40e3cc175db4049
Fry-Reality <Walle2511@gmail.com>
2024-05-14 17:16:04 +0200

    Test2
    also renamed the 1 one bc it was from the string calc

--- string_calculator/spec/string_calculator_spec.js
+++ string_calculator/spec/string_calculator_spec.js
@@ -4,9 +4,13 @@ describe('ALF Spaceship finder', function() {
 	});
 
 	//Test 1
-	it('should return 0 for an empty string', function() {
+	it('should return the lost forever message if there is no field', function() {
 		expect(spaceshipFinder.find('')).toEqual("Spaceship lost forever");
 	});
 
+	//Test 2
+	it('should return the correct result in a valid example', function () {
+		expect(spaceshipFinder.find('..........\n..........\n..........\n.......X..\n..........\n..........')).toEqual("[7, 2]");
+	});
 	
 });
--- string_calculator/src/string_calculator.js
+++ string_calculator/src/string_calculator.js
@@ -5,4 +5,13 @@ finder.prototype.find = function(map) {
 		return "Spaceship lost forever";
 	}
 
+	const rows = map.split('\n');
+	for (let rowIndex = rows.length - 1; rowIndex >= 0; rowIndex--) {
+		const index = rows[rowIndex].indexOf('X');
+		if (index !== -1) {
+			return `[${index}, ${rows.length - 1 - rowIndex}]`;
+		}
+	}
+	return "Spaceship lost forever";
+
 };
