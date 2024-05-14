49b316587d88328182f4089ab707093ba0aa7724
Fry-Reality <Walle2511@gmail.com>
2024-05-14 17:19:46 +0200

    Test3 + renaming
    renamed the files, added the 3 test. there won't be more commits regarding the finder.js bc it is already final

--- string_calculator/spec/finder_spec.js
+++ string_calculator/spec/finder_spec.js
@@ -13,4 +13,9 @@ describe('ALF Spaceship finder', function() {
 		expect(spaceshipFinder.find('..........\n..........\n..........\n.......X..\n..........\n..........')).toEqual("[7, 2]");
 	});
 	
+	//Test 3
+	it('should return the lost forever message if there is no spaceship in the field', function () {
+		expect(spaceshipFinder.find('..........\n..........\n..........\n..........\n..........\n..........')).toEqual("Spaceship lost forever");
+	});
+
 });
